4b695fc40a46f1b4721131dc9687272efa396ade
Strength9 <Strength9@users.noreply.github.com>
2023-01-06 18:45:49 +0000

    contact page css

--- template-parts/block/block_contactpage.php
+++ template-parts/block/block_contactpage.php
@@ -22,7 +22,7 @@ echo '<section '.$anchor.' class="'.$blockclass .'" '.$styleoutput.'>
 	 <div class="wcp-column full">
 	 <h1>Let’s Talk</h1>
 	 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-		<h4>What are you Looking For</h4>
+		<h4 class="contacttitle">What are you Looking For?</h4>
 	 </div>
 </div>
 	<div class="wcp-columns contactblock">
